92795018eb736a631e1792af2ed966133037764b
Chris Hyorok Lee <hl3838@columbia.edu>
2025-09-18 14:33:50 -0400

    date error checking

--- assignment2/part2,3,4/taskLogic.js
+++ assignment2/part2,3,4/taskLogic.js
@@ -36,7 +36,7 @@ function addTask(teammateName, task, date){
     const checkboxDetails = document.createElement("div");
     checkboxDetails.className = "checkbox-details";
 
-    const checkboxText = document.createElement("text");
+    const checkboxText = document.createElement("span");
     checkboxText.textContent = `Due: ${date || "No date set"}`;
 
     const checkbox = document.createElement("input");
@@ -47,7 +47,44 @@ function addTask(teammateName, task, date){
     individualTask.append(taskText, checkboxDetails);
     taskWrapper.appendChild(individualTask);
 }
+//2012-05-24 2025-09-18
+function compareDate(date, currDate){
+    if (!date) {
+        return false;
+    }
 
+    const inputDate = Number(date.slice(8,10));
+    const inputMonth = Number(date.slice(5,7));
+    const inputYear = Number(date.slice(0, 4));
+
+    const todaysDate = currDate.getDate();
+    const todaysMonth = currDate.getMonth()+1;
+    const todaysYear = currDate.getFullYear();
+
+    if (inputMonth < 1 || inputMonth > 12 || inputDate < 1 || inputDate > 31) {
+        return false;
+    }
+
+    if (inputYear > todaysYear) {
+        return true;
+    } 
+    else if (inputYear < todaysYear) {
+        return false;
+    } 
+    //same year cases
+    else {
+        if (inputMonth > todaysMonth) {
+            return true;
+        } else if (inputMonth < todaysMonth) {
+            return false;
+        } 
+        //same year, same month
+        else {
+            return inputDate >= todaysDate;
+        }  
+
+    }
+}
 
 //Add back the placeholder if all tasks are cleared
 function showPlaceholder(){
@@ -71,10 +108,19 @@ document.addEventListener("DOMContentLoaded", function(){
         const task = taskInput.value;
         const date = dateInput.value;
 
-        if (teammateName && task) {
+        /*make sure input date is after today's date*/
+        const currDate = new Date();
+        const checkDate = compareDate(date, currDate);
+        console.log(checkDate);
+        // make sure teammateName, task, date is chosen
+        if (teammateName && task && date && checkDate) {
             addTask(teammateName, task, date);
+            // Clear inputs after successful addition
+            taskInput.value = '';
+            dateInput.value = '';
+            selectElement.selectedIndex = 0; 
         } else {
-            alert("Please select a teammate and enter a task to continue");
+            alert("Please select a teammate, enter a task, and enter a correct date to continue");
         }
         
     });
